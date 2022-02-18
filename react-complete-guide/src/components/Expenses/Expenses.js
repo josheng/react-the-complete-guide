d68c22929cd8b0f336777f4d6ceab82808d8ae7e
Joshua Eng <engch28@gmail.com>
2022-02-18 15:26:34 +0800

    completed section 5

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
@@ -33,6 +34,7 @@ const Expenses = (props) => {
             date={expense.date}
           />
         ))} */}
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
